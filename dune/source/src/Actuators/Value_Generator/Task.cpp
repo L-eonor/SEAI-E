ab7473d89d335a691fbcffa653ab812595319600
FXGO98 <up201604934@fe.up.pt>
2020-12-21 16:13:04 +0000

    Changed UART test Task

--- dune/source/src/Actuators/Value_Generator/Task.cpp
+++ dune/source/src/Actuators/Value_Generator/Task.cpp
@@ -44,66 +44,92 @@ namespace Actuators
   {
     using DUNE_NAMESPACES;
 
-    /* //!Task arguments.
+    //!Task arguments.
     struct Arguments
     {
-      //! PRNG type.
-      std::string prng_type;
-      //! PRNG seed.
-      int prng_seed;
-      //! Mean temperature value.
-      float mean_value;
-      //! Standard deviation of temperature measurements.
-      double std_dev;
-    }; */
+      //! Zig Zag value generation.
+      bool zigzag;
+      //! Dispatch frequency.
+      double freq;
+      //! Increment for every cycle.
+      float increment;
+      //! Upper bound of generated values.
+      float upper_bound;
+      //! Lower bound of generated values.
+      float lower_bound;
+    };
     
     struct Task: public DUNE::Tasks::Periodic
     {
-      //! PRNG handle
-      //Random::Generator* m_prng;
       //! Task arguments.
-      //Arguments m_args;
+      Arguments m_args;
+      //! Current value to send to thruster 1
+      float m_t1_curr_value;
+      //! Current value to send to thruster 2
+      float m_t2_curr_value;
+      //! Current value to send to rudder 
+      float m_s1_curr_value;
+      //! Weather we're going up or down in zig zag mode
+      bool m_up;
+      //! Actuation Message to send to thruster
+      IMC::SetThrusterActuation m_thruster_1, m_thruster_2;
+      //! Actuation Message to send to rudder
+      IMC::SetServoPosition m_servo_1;
 
       Task(const std::string& name, Tasks::Context& ctx):
         DUNE::Tasks::Periodic(name, ctx)
         //m_prng(NULL)
-      {/*
-        param("Standard deviation", m_args.std_dev)
-        .description("Standard deviation of produced temperature")
-        .units(Units::DegreeCelsius)
+      {
+        param("Zig Zag Mode", m_args.zigzag)
+        .description("Weather to use RNG values or up and down values")
+        .defaultValue("true");
+
+        param("Dispatch Frequency", m_args.freq)
+        .description("Frequency at which task runs.")
+        .defaultValue("1.0");
+
+        param("Value Increment", m_args.increment)
+        .description("Value at which generated values increase in zig zag mode")
         .defaultValue("0.1");
 
-        param("PRNG Type", m_args.prng_type)
-        .defaultValue(Random::Factory::c_default);
+        param("Upper Bound", m_args.upper_bound)
+        .description("Upper bound of generated values")
+        .defaultValue("1.0");
 
-        param("PRNG Seed", m_args.prng_seed)
-        .defaultValue("-1");
+        param("Lower Bound", m_args.lower_bound)
+        .description("Upper bound of generated values")
+        .defaultValue("-1.0");
 
-        param("Mean value", m_args.mean_value)
-        .description("Mean value of produced temperature")
-        .units(Units::DegreeCelsius)
-        .defaultValue("25.0"); */
+
+        m_thruster_1.setSourceEntity(getEntityId());
+        m_thruster_1.id = 0;
+        m_thruster_2.setSourceEntity(getEntityId());
+        m_thruster_2.id = 1;
+        m_servo_1.setSourceEntity(getEntityId());
+        m_servo_1.id = 1;
       }
 
       void
       onEntityReservation(void)
       {
-        inf("Starting: %s", resolveEntity(getEntityId()).c_str());
+        setFrequency(m_args.freq);
+        m_t1_curr_value = m_args.upper_bound;
+        m_t2_curr_value = m_args.upper_bound;
+        m_s1_curr_value = m_args.upper_bound;
       }
 
       //! Aquire resources.
       void
       onResourceAcquisition(void)
       {
-        /*m_prng = Random::Factory::create(m_args.prng_type,
-                                         m_args.prng_seed);*/
+
       }
 
       //! Release resources.
       void
       onResourceRelease(void)
       {
-        //Memory::clear(m_prng);
+
       }
 
 
@@ -111,67 +137,92 @@ namespace Actuators
       void
       task(void)
       {
-        IMC::SetThrusterActuation thruster_1, thruster_2;
-        IMC::SetServoPosition servo_1;
         uint8_t msg_num;
         int value;
   
+        //Zig Zag mode
+        if (m_args.zigzag)
+        {
+          if (m_up)
+          {
+            m_t1_curr_value += m_args.increment;
+            m_t2_curr_value += m_args.increment;
+            m_s1_curr_value += m_args.increment;
 
-        while (!stopping())
+            if (m_t1_curr_value > m_args.upper_bound)
+            {
+              m_up = false;
+              m_t1_curr_value -= 2*m_args.increment;
+              m_t2_curr_value -= 2*m_args.increment;
+              m_s1_curr_value -= 2*m_args.increment;
+            }
+          }
+          else
+          {
+            m_t1_curr_value -= m_args.increment;
+            m_t2_curr_value -= m_args.increment;
+            m_s1_curr_value -= m_args.increment;
+
+            if (m_t1_curr_value < m_args.lower_bound)
+            {
+              m_up = true;
+              m_t1_curr_value += 2*m_args.increment;
+              m_t2_curr_value += 2*m_args.increment;
+              m_s1_curr_value += 2*m_args.increment;
+            }
+          }
+
+          m_thruster_1.value = m_t1_curr_value;
+          m_thruster_2.value = m_t2_curr_value;
+          m_servo_1.value    = m_s1_curr_value;
+
+          dispatch(m_thruster_1);
+          dispatch(m_thruster_2);
+          dispatch(m_servo_1);
+        }
+
+        // RNG mode
+        else
         {
           srand(time(NULL));
           msg_num = rand() % 3 + 1;
 
           if(msg_num == 1)
           {
-            thruster_1.id = 1;
             value = rand() % 101;
-            thruster_1.value = (((float)value * 0.01)-0.5)*2;  
-            thruster_1.setSourceEntity(getEntityId());
-            dispatch(thruster_1);
+            m_thruster_1.value = (((float)value * 0.01)-0.5)*2;  
+            dispatch(m_thruster_1);
           }
 
           if(msg_num == 2)
           {
-            thruster_1.id = 1;
             value = rand() % 101;
-            thruster_1.value = (((float)value * 0.01)-0.5)*2;
-            thruster_1.setSourceEntity(getEntityId());
+            m_thruster_1.value = (((float)value * 0.01)-0.5)*2;
 
-            servo_1.id = 1;
             value = rand() % 101;
-            servo_1.value = (((float)value * 0.01)-0.5)*2;
-            servo_1.setSourceEntity(getEntityId());
+            m_servo_1.value = (((float)value * 0.01)-0.5)*2;
 
-            dispatch(thruster_1);
-            dispatch(servo_1);
+            dispatch(m_thruster_1);
+            dispatch(m_servo_1);
           }
 
 
           if(msg_num == 2)
           {
-            thruster_1.id = 1;
             value = rand() % 101;
-            thruster_1.value = (((float)value * 0.01)-0.5)*2;
-            thruster_1.setSourceEntity(getEntityId());
+            m_thruster_1.value = (((float)value * 0.01)-0.5)*2;
 
-            servo_1.id = 1;
             value = rand() % 101;
-            servo_1.value = (((float)value * 0.01)-0.5)*2;
-            servo_1.setSourceEntity(getEntityId());
+            m_servo_1.value = (((float)value * 0.01)-0.5)*2;
 
-            thruster_2.id = 2;
             value = rand() % 101;
-            thruster_2.value = (((float)value * 0.01)-0.5)*2;
-            thruster_2.setSourceEntity(getEntityId());
+            m_thruster_2.value = (((float)value * 0.01)-0.5)*2;
 
-            dispatch(thruster_1);
-            dispatch(servo_1);
-            dispatch(thruster_2);
+            dispatch(m_thruster_1);
+            dispatch(m_servo_1);
+            dispatch(m_thruster_2);
           }
-          
-          Delay::wait(5.0);
-        }
+        }  
 
         return;
       }
